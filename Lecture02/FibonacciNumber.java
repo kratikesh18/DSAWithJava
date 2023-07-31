093b3be2ca5d8f5e406c228e80ae5b42e76204f7
kratikesh18 <kartikpachkawade@gmail.com>
2023-07-31 12:26:59 +0530

    Fibonacci series via loop performed

--- Lecture02/FibonacciNumber.java
+++ Lecture02/FibonacciNumber.java
@@ -1,7 +1,24 @@
 package com.DSAWithJava.Lecture02;
 
+import java.util.Scanner;
+
 public class FibonacciNumber {
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+
+        int prev = 0;
+        int next = 1;
+
+        int n = input.nextInt();
+        int count = 2;
+        while (count  <= n ){
+            int temp = next;
+            next = next + prev ;
+            prev = temp;
+            count++;
+        }
+
+        System.out.println("the fib num is : " + next );
     }
 }
