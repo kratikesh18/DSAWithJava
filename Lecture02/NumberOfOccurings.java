c74414d42b2daa19222d5836c7c3a0a12d6d81c8
kratikesh18 <kartikpachkawade@gmail.com>
2023-07-31 12:27:38 +0530

    Number of Occurings in given number Solved

--- Lecture02/NumberOfOccurings.java
+++ Lecture02/NumberOfOccurings.java
@@ -1,2 +1,19 @@
-package com.DSAWithJava.Lecture02;public class NumberOfOccurings {
+package com.DSAWithJava.Lecture02;
+
+public class NumberOfOccurings {
+    public static void main(String[] args) {
+        int n = 1457327417;
+        //count the occurings of 7
+        int count = 0 ;
+        int numberOfOccuring = 0 ;
+        while (n  > 0){
+            int lastDigit = n %10;
+            if(lastDigit == 7){
+                count++ ;
+            }
+            n = n/10;
+        }
+
+        System.out.println("the count of 7 in the n is : " + count);
+    }
 }
