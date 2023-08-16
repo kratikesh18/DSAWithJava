765b4a1a0ffd001c9c89b510fa35df858e73fb27
kratikesh18 <kartikpachkawade@gmail.com>
2023-08-16 17:22:42 +0530

    added test cases

--- Lecture09/BubbleSort.java
+++ Lecture09/BubbleSort.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int[] arr = {3, 1, 5, 4, 2};
+//        int[] arr = {3, 1, 5, 4, 2};
+        int[] arr = {5,4,3,2,1};
         System.out.println(Arrays.toString(arr));
 
         bubbleSort(arr);
