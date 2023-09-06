9be44e3cd6d604a45055bc10268689da17541c7d
kratikesh18 <kartikpachkawade@gmail.com>
2023-09-06 20:06:00 +0530

    Solved BubbleSort using Recursion

---
+++ Lecture17_PatternNdSorting/BubbleSortUsingRecursion.java
@@ -0,0 +1,34 @@
+package com.DSAWithJava.Lecture17_PatternNdSorting;
+
+import java.util.Arrays;
+
+public class BubbleSortUsingRecursion {
+    public static void main(String[] args){
+        int[] arr = {5,4,3,2,1};
+
+        System.out.println(Arrays.toString(arr));
+        bubbleSortRecursive(arr , arr.length-1 , 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSortRecursive(int[] arr , int row , int col){
+        //BaseCase
+        if(row == 0 ){
+            return;
+        }
+        if(col < row){
+            //cheking for swapping condition
+            if(arr[col] > arr[col+1]){  //ATTENTION TO SIGN HERE
+                //swapping logic
+                int temp = arr[col];
+                arr[col] = arr[col+1];
+                arr[col+1] = temp;
+            }
+            //recursive call for next column item
+            bubbleSortRecursive(arr , row , col+1);
+        }else{
+            //just a call for next row
+            bubbleSortRecursive(arr , row -1, 0);
+        }
+    }
+}
