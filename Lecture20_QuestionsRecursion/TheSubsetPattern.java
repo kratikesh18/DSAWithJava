c0bea6dd6769f262ceaae31714bc9e14a976f9ed
kratikesh18 <kartikpachkawade@gmail.com>
2023-09-20 23:21:35 +0530

    The Subset Pattern solved

--- Lecture20_QuestionsRecursion/TheSubsetPattern.java
+++ Lecture20_QuestionsRecursion/TheSubsetPattern.java
@@ -1,10 +1,17 @@
 package com.DSAWithJava.Lecture20_QuestionsRecursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TheSubsetPattern {
     public static void main(String[] args){
-        String str = "abc";
-        printSubsets("" , str);
+        String str = "123";
+//        printSubsets("" , str);
+
+//        printSubsets1("" , "abc");
+        System.out.println(listSubsets("" , "abc"));
     }
+
 
 
     static void printSubsets(String ans , String str){
@@ -17,5 +24,50 @@ public class TheSubsetPattern {
         //pick not pick pattern
         printSubsets(ans + ch , str.substring(1));
         printSubsets(ans , str.substring(1));
+    }
+
+    static void printSubsetsWithAscii(String ans , String str){
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = str.charAt(0);
+        //pick not pick pattern
+        printSubsetsWithAscii(ans + ch , str.substring(1));
+        printSubsetsWithAscii(ans , str.substring(1));
+        printSubsetsWithAscii(ans + (ch+0) , str.substring(1));
+    }
+
+
+    static void printSubsets1(String processed , String unprocessed ){
+        if(unprocessed.isEmpty()){
+            System.out.println(processed);
+            return;
+        }
+
+        char ch  = unprocessed.charAt(0);
+
+        //function calls
+        printSubsets1(processed , unprocessed.substring(1));
+        printSubsets1(  processed + ch, unprocessed.substring(1));
+    }
+
+
+    static List<String> listSubsets(String processed, String unprocessed){
+        if(unprocessed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        //extracting the character
+        char ch = unprocessed.charAt(0);
+
+        List<String> left = listSubsets(processed, unprocessed.substring(1));
+        List<String> right = listSubsets(processed + ch  , unprocessed.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
