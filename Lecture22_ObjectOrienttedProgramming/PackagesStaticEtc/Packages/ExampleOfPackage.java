535aa491ffc1233835e3d56a4ad5207f8de10e8c
kratikesh18 <kartikpachkawade@gmail.com>
2023-09-29 01:32:27 +0530

    Learned about packages in java

---
+++ Lecture22_ObjectOrienttedProgramming/PackagesStaticEtc/Packages/ExampleOfPackage.java
@@ -0,0 +1,17 @@
+package com.DSAWithJava.Lecture22_ObjectOrienttedProgramming.PackagesStaticEtc.Packages;
+
+    //Dont forget to use the static
+import static com.DSAWithJava.Lecture22_ObjectOrienttedProgramming.PackagesStaticEtc.Packages.OtherExample.greeting2;
+
+public class ExampleOfPackage {
+    public static void main(String[] args) {
+        greeting2();
+    }
+
+    public static void greeting(){
+        System.out.println("Hello Karikesh");
+    }
+
+
+}
+
