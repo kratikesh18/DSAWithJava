b9a3d670cf52152d12d7d1d78cd0bcf7f1f8d2a8
kratikesh18 <kartikpachkawade@gmail.com>
2023-10-01 17:33:19 +0530

    polymorphism revolves around the this Shape class

---
+++ Lecture22_ObjectOrienttedProgramming/FourPrinciples/Polymorphism/Shapes.java
@@ -0,0 +1,19 @@
+package com.DSAWithJava.Lecture22_ObjectOrienttedProgramming.FourPrinciples.Polymorphism;
+
+public class Shapes {
+
+
+    void area(){
+        System.out.println("I'm in the Shapes");
+    }
+
+    //if we comment out this parent class's method we can't acess the area which is method  of the other child class // only if the reference variable  is of tye parent
+    //type and the object is of type the child type that process will happen whil runtime
+
+
+    //creating the method of same name for experiment compile time polymorphism
+    int area(int length , int breadth){
+        return length* breadth;
+    }
+
+}
